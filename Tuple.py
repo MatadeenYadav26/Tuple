96448438ddd10aa812ef7ac8d2c871f7d5300e56
KING <matadeenyadav202@gmail.com>
2025-12-29 12:57:54 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,22 @@
+# a = (1)
+# b = ("b")
+# c = (1,2,"a")
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+#Tuple Traversing:
+
+# a = (10,20,30,40,55,55,55,60,60,60,60,60,60,54,56,77,8,88,76,54)
+
+# for i in a:
+#     print(i)
+
+# for i in range(len(a)):
+#     print(a[i])
+
+
+# Tuple Functions/Methods: 
+# print(a.count(60))
+# print(a.index(55))
